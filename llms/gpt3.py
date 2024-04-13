9de79745c2c5ac063fd8d377a8b3a78a3599cda4
shreyash2106 <shreyash2106@berkeley.edu>
2024-04-13 17:34:23 -0400

    GPT 3.5 setup

--- llms/gpt3.py
+++ llms/gpt3.py
@@ -1,16 +1,26 @@
 import openai
+import time
+import sys
 
 class GPT3Model:
-    def __init__(self, api_key):
+    def __init__(self, api_key, model, temperature):
         openai.api_key = api_key
+        self.model = model
+        self.temperature = temperature
 
     def generate_response(self, prompt):
-        response = openai.Completion.create(
-            engine="text-davinci-002",  # For GPT-3.5, specify the appropriate engine
-            prompt=prompt,
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-        return response.choices[0].text.strip()
+        response = None
+        received = False
+        while not received:
+            try:
+                response = openai.Completion.create(engine=self.model, prompt=prompt, temperature=self.temperature, stop=None, n=1)
+                received = True
+            except:
+                error = sys.exc_info()[0]
+                if error == openai.error.InvalidRequestError: # something is wrong: e.g. prompt too long
+                    print(f"InvalidRequestError\nPrompt passed in:\n\n{prompt}\n\n")
+                    assert False
+
+                print("API error:", error)
+                time.sleep(1)
+        return response
