040275830ab72161c2a13183f807df916c3d01ec
shreyash2106 <shreyash2106@berkeley.edu>
2024-04-14 19:25:37 -0400

    Use LLama tokenizer

--- src/llms/llama.py
+++ src/llms/llama.py
@@ -1,12 +1,12 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
 
 
 class LLamaModel:
-    def __init__(self, model_name="Maykeye/TinyLLama-v0"):
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForCausalLM.from_pretrained(model_name)
+    def __init__(self, model_name="openlm-research/open_llama_3b"):
+        tokenizer = LlamaTokenizer.from_pretrained(model_name)
+        model = LlamaForCausalLM.from_pretrained(model_name)
         self.generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
     def generate_response(self, prompt):
-        result = self.generator(prompt, max_length=150, num_return_sequences=1)
+        result = self.generator(prompt, max_length=1000, num_return_sequences=1, truncation=True)
         return result[0]["generated_text"].strip()
